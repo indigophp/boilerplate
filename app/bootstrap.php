3e5e9315b041247df69591091d5354bd2b846e0f
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-04-19 17:41:48 +0200

    Adds advanced modifications

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,18 +9,40 @@
  * file that was distributed with this source code.
  */
 
+use Dotenv\Dotenv;
 use League\Container\Container;
 use Proton\Application;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This file is responsible for creating the application and
  * loading all resources which are necessary for the application to run.
  *
  * These include:
+ *  - Environment loading
  *  - Environment check
  *  - Dependency container setup
  *  - Configuration loading (optional)
  */
+
+/**
+ * Loading environment
+ *
+ * This should be done right before the application is loaded, since the application relies on the environment
+ */
+$dotenv = new Dotenv;
+
+// To avoid the overhead caused by file loading, this is optional in production
+if (defined('APP_ENV') and APP_ENV == 'development') {
+    $dotenv->load(APP_ROOT);
+}
+
+/**
+ * Checking environment
+ *
+ * The environment is already loaded, now it should be checked
+ */
+$dotenv->required(['APP_ROOT', 'APP_CONFIG']);
 
 /**
  * Setting up dependency container
@@ -33,10 +55,23 @@ $container = new Container($diConfig);
  */
 $app = new Application;
 $app->setContainer($container);
+$app['env'] = $dotenv;
+
+/**
+ * Loading configuration
+ */
+if (is_file($configFile = getenv('APP_CONFIG'))) {
+    foreach (Yaml::parse($configFile) as $config => $value) {
+        $app->setConfig($config, $value);
+    }
+}
 
 // The application path is protected from being overwritten
 // Should be placed after configuration loading
 $app->setConfig('path', __DIR__);
+
+// Set locale based on environment
+setlocale(LC_ALL, getenv('LANG'));
 
 // Adding routes
 $app->get('/', 'controller::index');
--- public/index.php
+++ public/index.php
@@ -10,9 +10,12 @@
  */
 
 defined('APP_ROOT') or define('APP_ROOT', realpath(__DIR__.'/../'));
+putenv('APP_ROOT='.APP_ROOT);
+
+defined('APP_ENV') or define('APP_ENV', getenv('APP_ENV') ?: 'development');
 
 require APP_ROOT.'/vendor/autoload.php';
 
 $app = require APP_ROOT.'/app/bootstrap.php';
 
-$app->run();
+Stack\run($app);
